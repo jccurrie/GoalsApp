5bc1f1fcd471929b7f2cb503f263d1c85de03554
Julian Currie <jccurrie@uab.edu>
2023-01-02 12:22:59 -0600

    Initialization of project

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>hello</Text>
+      <Button title="Push me"/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#',
     alignItems: 'center',
     justifyContent: 'center',
   },
